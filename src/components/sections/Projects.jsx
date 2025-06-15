d5c0320aa6c01c0eacc291d68eee1c36607ef0e3
harshitkkush <harshitkkush@gmail.com>
2025-06-16 01:10:28 +0530

    first commit

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,171 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Spotify Clone </h3>
+              <p className="text-gray-400 mb-4">
+              Built a Spotify clone using HTML, CSS and JavaScript, replicating core features like a responsive music player, playlist
+              management, and a sleek UI.Integrated JavaScript to implement interactive features such as play/pause
+              controls,seek bar functionality, volume adjustments, and dynamic playlist updates.Tested the clone extensively on
+              multiple browsers and screen sizes to ensure consistent performance and compatibilty.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML", "CSS", "JavaScript"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Credit Card Fraud Detection</h3>
+              <p className="text-gray-400 mb-4">
+                ML-powered data visualization platform with predictive analytics
+                and interactive reports.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "Numpy", "Pandas", "Streamlit"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Recipe Website</h3>
+              <p className="text-gray-400 mb-4">
+              Developed a recipe website using ReactJS and MealDB API, enabling users to browse, search, and filter recipes
+              seamlessly.Utilized ReactJS components and hooks to create a responsive, modular and efficient user interface that
+              works across all devices.Integrated MealDB API to fetch real-time recipe data including detailedingredients,
+              instructions, and images enhancing user engagement.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["ReactJs", "HTML", "Tailwind", "MealDB"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Real-Time Tracker using Socket.IO</h3>
+              <p className="text-gray-400 mb-4">
+              Developed a real-time location tracking system using Socket.IO, Node.js, and Google Maps API, enabling live updates
+              for moving objects.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Google Maps API"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
